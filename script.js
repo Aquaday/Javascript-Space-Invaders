914299546d8ada9e37e393e41feca736f52fb333
Aquaday <martin991940@hotmail.com>
2025-05-20 10:06:00 +0200

    litt kommentarer

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const grid = document.querySelector('.grid');
 const resultDisplay = document.querySelector('.results');
 const width = 15;
+//brukes for å lagre verdiene til de fjernede aliens.
 const aliensRemoved = []
+
+//velger hvilken div boks som spilleren begynner på
 let currentShooterIndex = 202
 let invadersID
+
 let isGoingRight = true
 let direction = 1
 let results = 0
 
+//lager spill brettet
 for (let i = 0; i < width * width; i++) {
   const square = document.createElement('div');
   grid.appendChild(square);
@@ -15,11 +20,14 @@ for (let i = 0; i < width * width; i++) {
 
 const squares = Array.from(document.querySelectorAll('.grid div'));
 
+//velger hvilken div aliens skal begynne på
 const alienInvaders = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30, 31, 32,
   33, 34, 35, 36, 37,38,39
 ];
 
+
+//plasserer aliens i valgte divs, så lenge invaders ikke ligger i aliens removed array.
 function draw() {
   for (let i = 0; i < alienInvaders.length; i++) {
     if (!aliensRemoved.includes(i)) {
@@ -34,6 +42,8 @@ squares[currentShooterIndex].classList.add("shooter")
 
 function moveShooter(e) {
   squares[currentShooterIndex].classList.remove("shooter")
+
+  //ser om e.key er venstre eller høyre, og beveger den til høyre eller venstre, så lenge den ikke er ved siden av en vegg.
   switch(e.key) {
     case "ArrowLeft": 
       if (currentShooterIndex % width !==0) currentShooterIndex -= 1
